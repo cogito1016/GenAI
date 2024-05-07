d9343ee7c903ac801196df01a21324c72010c2ee
cogito1016 <cogito1016@naver.com>
2024-05-08 06:59:54 +0900

    inch->cm changer

---
+++ python/basic/chapter_1_2/ex_inch_to_cm_changer.py
@@ -0,0 +1,10 @@
+inchStr = input("인치를 입력하세요 > ")
+inchFloat = float(inchStr)
+cm = inchFloat * 2.54
+
+#print(inchStr + " inch = "+str(cm)+" cm")
+print(inchStr," inch = ",cm," cm")
+
+# RESULT
+# 인치를 입력하세요1
+# 1 inch = 2.54 cm
