515b9e9dab5abd542a697c5ec1a8f7f94af20a84
cogito1016 <cogito1016@naver.com>
2024-05-09 06:36:49 +0900

    문자열 함수 is00, 문자열구성 검사

---
+++ python/basic/chapter_1_2/string_function_is00.py
@@ -0,0 +1,17 @@
+# 문자열의 구성 파악하기 is00()
+## isalnum-알파벳또는숫자인지
+## isalpha-알파벳인지
+## isdentifier-식별자로사용할수있는지
+## isdigit-정수인지
+## isspace-공백인지
+## islower-소문자인지
+## isupper-대문자인지
+print(" ".isspace())
+print("hello".isspace())
+print("hello".islower())
+print("1q2W".isalnum())
+## RESULT
+# True
+# False
+# True
+# True
