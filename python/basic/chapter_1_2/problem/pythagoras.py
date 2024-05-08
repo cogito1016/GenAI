3974444b96576f89e1587285c1679a963859c320
cogito1016 <cogito1016@naver.com>
2024-05-09 06:57:39 +0900

    피타고라스 정리

---
+++ python/basic/chapter_1_2/problem/pythagoras.py
@@ -0,0 +1,10 @@
+# 피타고라스정리
+under_line_length = int(input("밑변의 길이를 입력하세요 > "))
+height = int(input("높이의 길이를 입력하세요 > "))
+
+print(f"빗변의 길이는 {(under_line_length**2 + height**2)**(0.5)}")
+
+## REUSLT 
+# 밑변의 길이를 입력하세요 > 3
+# 높이의 길이를 입력하세요 > 4
+# 빗변의 길이는 5.0
