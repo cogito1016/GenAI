9caf4eee33aec9670f94d56169d5ab1c12694471
cogito1016 <cogito1016@naver.com>
2024-05-10 08:13:37 +0900

    파이썬 not, and, or 연산자

---
+++ python/basic/chapter_1_2/conditional_state.py
@@ -0,0 +1,13 @@
+
+# 파이썬은 ! -> not , && -> and , || -> or
+
+print(True) #t
+print(False) #f
+print(not True) #f
+print(not False) #t 
+print(True and True) #t
+print(True and False) #f
+print(False and False) #f 
+print(True or True) #t
+print(True or False) #t
+print(False or False) #f
