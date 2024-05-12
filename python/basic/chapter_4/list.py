202547d9e4776d828b7836b843889377dec06232
cogito1016 <cogito1016@naver.com>
2024-05-13 06:39:08 +0900

    리스트 범위선택 [n:m]

--- python/basic/chapter_4/list.py
+++ python/basic/chapter_4/list.py
@@ -1,7 +1,9 @@
-array = [1,2,1.1,"안녕",True]
+array = [1, 2, 1.1, "안녕", True]
 print(array)
 print(type(array))
 
 ##RESULT
 # [1, 2, 1.1, '안녕', True]
 # <class 'list'>
+
+print(array[1:4]) # 1번인덱스부터(0부터 시작하는 인덱스 기준) 4번째숫자까지(1부터 시작하는 개수 기준)
