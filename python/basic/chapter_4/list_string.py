01eee089e7fb173a643dc4bfe237e0f79b6647d0
cogito1016 <cogito1016@naver.com>
2024-05-13 06:51:18 +0900

    리스트 합침방법, 원본유지방법과 원본변화방법

--- python/basic/chapter_4/list_string.py
+++ python/basic/chapter_4/list_string.py
@@ -8,3 +8,22 @@ print(f"len(array_1) = {len(array_1)}")
 # array_1+array_2 = [1, 2, 3, 4, 5, 6]
 # array_1*2 = [1, 2, 3, 1, 2, 3]
 # len(array_1) = 3
+
+# 리스트합침방법(원본유지, 원본변화)
+## 원본유지
+print(array_1, array_2)
+print(array_1 + array_2)
+print(array_1)
+print(array_2)
+## 원본파괴
+print(array_1.extend(array_2))
+print(array_1)
+print(array_2)
+## RESULT
+# [1, 2, 3] [4, 5, 6]
+# [1, 2, 3, 4, 5, 6]
+# [1, 2, 3]
+# [4, 5, 6]
+# None
+# [1, 2, 3, 4, 5, 6]
+# [4, 5, 6]
