9a97e01d60ca1e5dfce88eeefec991369e3dc8af
cogito1016 <cogito1016@naver.com>
2024-05-13 06:40:18 +0900

    음수인덱스로 리스트 뒷  요소부터 접근

--- python/basic/chapter_4/list.py
+++ python/basic/chapter_4/list.py
@@ -7,3 +7,7 @@ print(type(array))
 # <class 'list'>
 
 print(array[1:4]) # 1번인덱스부터(0부터 시작하는 인덱스 기준) 4번째숫자까지(1부터 시작하는 개수 기준)
+
+# 음수로 뒤에서 부터 접근 (문자열과 마찬가지)
+print(array[-1]) # True
+print(array[-2]) # "안녕"
