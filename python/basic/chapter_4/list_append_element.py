2a87a5fc4f39237b2521f10ec28a62238d07e39a
cogito1016 <cogito1016@naver.com>
2024-05-13 06:48:12 +0900

    리스트에 요소 삽입

---
+++ python/basic/chapter_4/list_append_element.py
@@ -0,0 +1,11 @@
+# 리스트에 요소 추가 insert append
+array = [1,2,3]
+## append는 뒤에 추가
+array.append(4)
+print(array)
+## insert는 특정 위치에 추가 insert(인덱스,값)
+array.insert(1,8) 
+print(array)
+## RESULT
+# [1, 2, 3, 4]
+# [1, 8, 2, 3, 4]
