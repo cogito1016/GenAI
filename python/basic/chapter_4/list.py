0c0527df8e86049a2e875dabfa2430b06e55a768
cogito1016 <cogito1016@naver.com>
2024-05-13 06:44:31 +0900

    리스트와 문자열, 동일하게 사용되는 연산자 + * len

--- python/basic/chapter_4/list.py
+++ python/basic/chapter_4/list.py
@@ -1,13 +1,15 @@
+#1
 array = [1, 2, 1.1, "안녕", True]
 print(array)
 print(type(array))
-
 ##RESULT
 # [1, 2, 1.1, '안녕', True]
 # <class 'list'>
 
+#2
 print(array[1:4]) # 1번인덱스부터(0부터 시작하는 인덱스 기준) 4번째숫자까지(1부터 시작하는 개수 기준)
 
+#3
 # 음수로 뒤에서 부터 접근 (문자열과 마찬가지)
 print(array[-1]) # True
 print(array[-2]) # "안녕"
---
+++ python/basic/chapter_4/list_string.py
@@ -0,0 +1,10 @@
+# 문자열과 리스트에서 동일하게 사용할 수 있는 연산자
+array_1 = [1,2,3]
+array_2 = [4,5,6]
+print(f"array_1+array_2 = {array_1+array_2}")
+print(f"array_1*2 = {array_1*2}")
+print(f"len(array_1) = {len(array_1)}")
+## RESULT
+# array_1+array_2 = [1, 2, 3, 4, 5, 6]
+# array_1*2 = [1, 2, 3, 1, 2, 3]
+# len(array_1) = 3
