f7cfa8447842b3fa928d2039f0b5a78bb832505a
cogito1016 <cogito1016@naver.com>
2024-05-14 06:33:22 +0900

    리스트 특정 값 제거 & 리스트 초기화

--- python/basic/chapter_4/list_remove_element.py
+++ python/basic/chapter_4/list_remove_element.py
@@ -1,4 +1,4 @@
-#인덱스로 제거하기
+# 인덱스로 제거하기 del, pop
 num_list = [1,2,3,4,5]
 print(num_list)
 del num_list[2]
@@ -10,3 +10,18 @@ print(num_list)
 # [1, 2, 3, 4, 5]
 # [1, 2, 4, 5]
 # [1, 2, 4]
+
+# 값으로 제거하기 remove
+num_list_2 = [1,2,1,2,3]
+print(num_list_2)
+num_list_2.remove(2)
+print(num_list_2)
+## RESULT
+# [1, 2, 1, 2, 3]
+# [1, 1, 2, 3]
+
+# 리스트 요소 전부삭제 clear
+num_list_2.clear()
+print(num_list_2)
+## RESULT
+# []
