ecb81419290c67abaeb60ca75bef7f47ac61f291
cogito1016 <cogito1016@naver.com>
2024-05-14 06:36:47 +0900

    리스트 정렬, 역정렬

---
+++ python/basic/chapter_4/list_sort.py
@@ -0,0 +1,10 @@
+list_1 = [1,3,4,6,1,2,3,1,]
+print(list_1)
+list_1.sort()
+print(list_1)
+list_1.sort(reverse=True)
+print(list_1)
+## REUSLT
+# [1, 3, 4, 6, 1, 2, 3, 1]
+# [1, 1, 1, 2, 3, 3, 4, 6]
+# [6, 4, 3, 3, 2, 1, 1, 1]
