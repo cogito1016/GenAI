c552b3d73a11fbb034829a05713726670935e6d8
cogito1016 <cogito1016@naver.com>
2024-05-29 23:23:33 +0900

    ValidParentheses - 떨어진 괄호도 처리 (스택)

--- python/basic/stack/stack.py
+++ python/basic/stack/stack.py
@@ -3,3 +3,7 @@ print(arr)
 arr.append(4)
 print(arr)
 print(arr.pop())
+
+arr_2 = list("hihello")
+print(arr_2)
+print(len(arr_2))
--- python/problem/ValidParentheses.py
+++ python/problem/ValidParentheses.py
@@ -5,22 +5,24 @@ class Solution(object):
         :rtype: bool
         """
         result = True
-        target = ""
-        for c in s:
-            if target == c:
-                target = ""
+        stack = list(s)
+        calcul_stack = []
+        while(len(stack)!=0):
+            c = stack.pop()
+            if len(calcul_stack) ==0:
+                calcul_stack.append(c)
                 continue
-            elif target != "":
-                result = False
-                break
 
-            if c == "{":
-                target = "}"
-            elif c == "[":
-                target = "]"
-            elif c =="(":
-                target = ")"
+            peek = calcul_stack[len(calcul_stack) - 1]
+            if (c == "[" and peek == "]") or (c == "(" and peek == ")") or (c == "{" and peek == "}"):
+                calcul_stack.pop()
+                continue
+            calcul_stack.append(c)
+
+        if len(calcul_stack)!=0:
+            result = False
+
         return result
 
 s = Solution()
-print(s.isValid("()[}"))
+print(s.isValid("()[{)]"))
