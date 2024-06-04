520cfd90998dd170b4f2213300e67613e02ac874
cogito1016 <cogito1016@naver.com>
2024-06-04 22:29:47 +0900

    refactor: Hashmap 기능구현

--- python/basic/hashSet/HashMap.py
+++ python/basic/hashSet/HashMap.py
@@ -1,6 +1,7 @@
 class MyHashMap(object):
 
     def __init__(self):
+        self.map = {}
 
     def put(self, key, value):
         """
@@ -8,21 +9,33 @@ class MyHashMap(object):
         :type value: int
         :rtype: None
         """
+        self.map[key] = value
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
+        if key in self.map:
+            return self.map[key]
+        return -1
 
     def remove(self, key):
         """
         :type key: int
         :rtype: None
         """
+        if key in self.map:
+            del self.map[key]
 
 # Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+myHashMap = MyHashMap();
+
+myHashMap.put(1, 1); # The map is now [[1,1]]
+myHashMap.put(2, 2); # The map is now [[1,1], [2,2]]
+myHashMap.get(1);    # return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3);    # return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); # The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2);    # return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); # remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2);    # return -1 (i.e., not found), The map is now [[1,1]]
