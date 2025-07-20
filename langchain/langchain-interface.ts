9c8d64ca8cd2041a6fca42bce0849bb96a60d378
김재형 <cogito1016@naver.com>
2025-07-20 20:33:11 +0900

    feat: 랭체인 인터페이스 - 시스템메세지

--- langchain/langchain-interface.ts
+++ langchain/langchain-interface.ts
@@ -1,13 +1,13 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 import 'dotenv/config';
-import {HumanMessage} from "@langchain/core/messages";
+import {HumanMessage, SystemMessage} from "@langchain/core/messages";
 
 const model = new ChatGoogleGenerativeAI({
     model : 'gemini-2.5-flash',
     apiKey : process.env.GEMINI_API_KEY
 })
 
-const prompt = [new HumanMessage('한국의 수도는 어디인가요?')]
+const prompt = [new SystemMessage('당신은 문장 끝에 하트 이모지를 붙여 대답하는 사랑스런 어시스턴트입니다.'),new HumanMessage('한국의 수도는 어디인가요?')]
 const response = await model.invoke(prompt)
 
 console.log(response)
