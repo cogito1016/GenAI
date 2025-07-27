c0175f7c9c49a811d5c5d124738c1afeb19db95e
cogito1016 <cogito1016@naver.com>
2025-07-27 23:49:49 +0900

    feat: 청크단위분활

--- langchain/7.document-to-text-transform.ts
+++ langchain/7.document-to-text-transform.ts
@@ -1,6 +1,7 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 import 'dotenv/config';
 import {TextLoader} from 'langchain/document_loaders/fs/text';
+import {RecursiveCharacterTextSplitter} from "@langchain/textsplitters";
 
 const model = new ChatGoogleGenerativeAI({
     model : 'gemini-2.5-flash',
@@ -8,5 +9,14 @@ const model = new ChatGoogleGenerativeAI({
 });
 
 const loader = new TextLoader('./context-test.txt')
+
+//문서 로드
 const docs = await loader.load();
-console.log(docs)
+
+//문서 청크단위로 분활
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize:1000,
+    chunkOverlap:200,
+})
+const splittedDocs = await splitter.splitDocuments(docs)
+console.log(splittedDocs)
